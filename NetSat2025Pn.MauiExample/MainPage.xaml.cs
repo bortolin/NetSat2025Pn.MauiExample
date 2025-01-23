4cee42c73a2db94f12b9fc2db4a1bcacf45d1ab1
Marco Bortolin <m.bortolin@Mac.Home>
2025-01-23 23:26:48 +0100

    Refactor

--- NetSat2025Pn.MauiExample/MainPage.xaml.cs
+++ NetSat2025Pn.MauiExample/MainPage.xaml.cs
@@ -8,27 +8,30 @@ namespace NetSat2025Pn.MauiExample;
 
 public partial class MainPage : ContentPage
 {
-
+    class SelectedNode
+    {
+        public int Index { get; set; }
+        public int Value { get; set; }
+    }
+    
+    //Oggetto che rappresenta il nodo selezionato
     SelectedNode? selectedNode;
 
 	public MainPage()
 	{
 		InitializeComponent();
+        
+        //Necessario per poter chimare chimare il metodo SetSelectedNode da javascript
+        //Specifica il target per l'invocazione di metodi
         hybridWebView.SetInvokeJavaScriptTarget(this);
 	}
 
-    public void SetSelectedNode(int index, int value)
-    {
-        selectedNode = new SelectedNode { Index = index, Value = value };
-        Slider.IsEnabled = true;
-        Slider.Value = selectedNode.Value;
-    }
-
+    //Evento scatenato quando viene ricevuto un messaggio da javascript
 	private void OnHybridWebViewRawMessageReceived(object sender, HybridWebViewRawMessageReceivedEventArgs e)
     {
         if (e.Message != null)
         {
-            //deserializzo il messaggio ricevuto in due valori interi Index e Value
+            //Deserializzo il messaggio ricevuto da javascript per ottenere l'indice e il valore del nodo selezionato
             selectedNode = JsonSerializer.Deserialize<SelectedNode>(e.Message);
             if (selectedNode != null) {
                 Slider.IsEnabled = true;
@@ -38,6 +41,28 @@ public partial class MainPage : ContentPage
         }
 	}
 
+    //Invio un messaggio a javascript per cambiare il tipo di grafico
+    private void ButtonBars_Clicked(object sender, EventArgs e)
+    {
+        hybridWebView.SendRawMessage("bar");
+    }
+
+    //Invio un messaggio a javascript per cambiare il tipo di grafico
+    private void ButtonLines_Clicked(object sender, EventArgs e)
+    {
+        hybridWebView.SendRawMessage("line");
+    }
+
+    //Metodo chiamato da javascript per settare il nodo selezionato
+    //Modalità alternativa all'uso di SendRawMessage
+    public void SetSelectedNode(int index, int value)
+    {
+        selectedNode = new SelectedNode { Index = index, Value = value };
+        Slider.IsEnabled = true;
+        Slider.Value = selectedNode.Value;
+    }
+
+    //Quando il valore dello slider cambia, invio il nuovo valore al nodo selezionato chimanao il metodo setData di javascript
     private async void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
     {
         var value = (int)e.NewValue;
@@ -45,20 +70,10 @@ public partial class MainPage : ContentPage
         if (selectedNode != null)
         {
             var retVal = await hybridWebView.InvokeJavaScriptAsync("setData",
-            HybridSampleJsContext.Default.Boolean, // JSON serialization info for return value
+            HybridSampleJsContext.Default.Boolean, // JSON serialization info del valore di ritorno
             [selectedNode.Index,value], // Parameter values
-            [HybridSampleJsContext.Default.Int32, HybridSampleJsContext.Default.Int32]); // JSON serialization info for each parameter
+            [HybridSampleJsContext.Default.Int32, HybridSampleJsContext.Default.Int32]); // JSON serialization info per ogni parametro
         }
-    }
-
-    private void ButtonBars_Clicked(object sender, EventArgs e)
-    {
-        hybridWebView.SendRawMessage("bar");
-    }
-
-    private void ButtonLines_Clicked(object sender, EventArgs e)
-    {
-        hybridWebView.SendRawMessage("line");
     }
 }
  
@@ -70,8 +85,3 @@ internal partial class HybridSampleJsContext : JsonSerializerContext
     // for trimmed builds.
 }
 
-class SelectedNode
-{
-    public int Index { get; set; }
-    public int Value { get; set; }
-}
